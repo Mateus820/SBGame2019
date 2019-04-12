ef284c591da70e2c62b5a487c51fc7e1ee5c8dc2
Aluno <aluno@naverecife.local>
2019-04-12 13:29:42 -0300

    My First Commit

---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool jumpVerify;
+
+    void Start()
+    {
+        jumpVerify = false;
+    }
+
+    void Update(){
+        if(Input.GetButtonDown("Jump") && jumpVerify){
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(speed * x, rb.velocity.y);        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Ground"){
+            jumpVerify = true;
+        }    
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Ground"){
+            jumpVerify = false;
+        }    
+    }
+}
